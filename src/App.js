c080d33f94279028b248d97b581945f566917167
Your Name <you@example.com>
2023-01-02 21:54:39 -0700

    "airbnb clone project, Navbar complete"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Navbar />
     </div>
   );
 }
