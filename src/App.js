5bd6403f1c79b54595172ac93915fc93529d9952
Your Name <you@example.com>
2023-01-02 22:43:08 -0700

    "Finished hero and card components and added CSS"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Main from "./components/Main";
+import Card from "./components/Card";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 
@@ -10,6 +11,8 @@ function App() {
     <div className="App">
       {/* <header className="App-header"> */}
       <Navbar />
+      <Hero />
+      <Card />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
